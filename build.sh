e2322c9a8d09c65720e9ce54e53ac495d60251bb
Duncan de Boer <duncan.d.boer@gmail.com>
2025-03-12 16:15:50 +0100

    added verification of composer dependencies

--- build.sh
+++ build.sh
@@ -2,9 +2,11 @@
 
 set -e
 
-rm -rf app/var/cache/*
-composer --working-dir=app install --no-dev --optimize-autoloader --no-interaction --no-progress -q
+rm -rf app/var/cache/prod
+sleep 1
 composer --working-dir=app dump-env prod
+composer --working-dir=app install --no-dev --optimize-autoloader --no-interaction --no-progress
 box compile --working-dir=app
+rm app/.env.local.php
 composer --working-dir=app install --no-progress -q
 bin/spc micro:combine bin/php-console-app --output bin/php-console-bin
